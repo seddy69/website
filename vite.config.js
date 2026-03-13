d324c7482c644657fee28d46dfc9b9af97ae4a89
seddy69 <118023428+seddy69@users.noreply.github.com>
2026-03-13 12:43:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 
   // ── BASE URL ──────────────────────────────────────────────────────────────
   // If you're using a custom domain (e.g. tristateislamic.center):
-  base: '/',
+  base: '/website/',
   //
   // If you're deploying to a GitHub Pages subfolder (username.github.io/repo):
   // base: '/your-repo-name/',
